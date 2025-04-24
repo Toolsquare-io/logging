a3fd9019697cc882f1286cda6f01005974a28133
Bram Esposito <bram@bramesposito.com>
2025-04-24 15:29:39 +0200

    remove specific methods in Base class

--- src/logOutputLogger.h
+++ src/logOutputLogger.h
@@ -5,8 +5,4 @@ class logOutputLogger {
 public:
     virtual ~logOutputLogger() = default;
     virtual bool output(const char *, const loggingLevel) = 0;
-    virtual bool debug(const char *) = 0;
-    virtual bool info(const char *) = 0;
-    virtual bool warning(const char *) = 0;
-    virtual bool error(const char *) = 0;
 };
